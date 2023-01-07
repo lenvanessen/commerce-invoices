4fa838d2603092456afcd55b31fbb36818a77e10
Len van Essen <len@wndr.digital>
2023-01-07 14:20:03 +0100

    Craft 4

--- src/models/FakePdf.php
+++ src/models/FakePdf.php
@@ -2,9 +2,19 @@
 
 namespace lenvanessen\commerce\invoices\models;
 
-class FakePdf
+use craft\commerce\elements\Order;
+use craft\commerce\models\Pdf;
+use lenvanessen\commerce\invoices\CommerceInvoices;
+
+class FakePdf extends Pdf
 {
-    public function getRenderLanguage($order)
+    public function __construct($config = [])
+    {
+        $this->templatePath = CommerceInvoices::getInstance()->getSettings()->pdfPath;
+        parent::__construct($config);
+    }
+
+    public function getRenderLanguage(Order $order = null): string
     {
         return \Craft::$app->getSites()->getPrimarySite()->language;
     }
